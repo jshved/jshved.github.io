04dc2eb683174c9dea0bd39c3d98b349da754cbb
jshved <89394151+jshved@users.noreply.github.com>
2021-10-12 17:49:19 -0400

    Add files via upload

---
+++ MNISTweights.py
@@ -0,0 +1,85 @@
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print ("X_train.shape: {}".format(X_train.shape))
+print ("y_train.shape: {}".format(y_train.shape))
+print ("X_test.shape: {}".format(X_test.shape))
+print ("y_test.shape: {}".format(y_test.shape))
+
+
+X_train.shape: (60000, 28, 28)
+y_train.shape: (60000,)
+X_test.shape: (10000, 28, 28)
+y_test.shape: (10000,)
+
+
+
+plt.subplot(161)
+plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
+plt.subplot(162)
+plt.imshow(X_train[5], cmap=plt.get_cmap('gray'))
+plt.subplot(163)
+plt.imshow(X_train[7], cmap=plt.get_cmap('gray'))
+plt.subplot(164)
+plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
+plt.subplot(165)
+plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
+plt.subplot(166)
+plt.imshow(X_train[13], cmap=plt.get_cmap('gray'))
+
+
+X_train.shape
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from keras.utils import np_utils
+
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+# X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+# X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+model = Sequential()
+
+# model.add(Dense(num_pixels, input_dim=num_pixels, activation='relu'))
+# model.add(Dense(num_classes, activation='softmax'))
+
+model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(15, (3, 3), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=30, batch_size=200, verbose=2)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+
+# Save whole model for download
+model.save("model.h5")
+
